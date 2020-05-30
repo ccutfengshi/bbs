b419e7b0b3ad23b1a24bc1a3fd61ad166ba91fd0
ccutfengshi <2524734034@qq.com>
2020-05-30 19:07:37 +0800

    引入新的插件：lombok 向数据表中添加存储头像的字段，并测试图像路径能否存入数据库中

--- src/main/java/com/ccut/bbs/dto/GithubUser.java
+++ src/main/java/com/ccut/bbs/dto/GithubUser.java
@@ -1,40 +1,11 @@
 package com.ccut.bbs.dto;
 
+import lombok.Data;
+
+@Data
 public class GithubUser {
     private String name;
     private Long id;  //防止用户暴增的时候数组越界，所以给long
     private String bio;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    @Override
-    public String toString() {
-        return "GithubUser{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", bio='" + bio + '\'' +
-                '}';
-    }
+    private String avatar_url; //获取头像
 }
--- src/main/java/com/ccut/bbs/mapper/UserMapper.java
+++ src/main/java/com/ccut/bbs/mapper/UserMapper.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 //解决mapper注入时显示could not autowire
 @Component(value = "userMapper")
 public interface UserMapper {
-    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified,avatar_url) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified},#{avatarUrl})")
     void insert(User user);
 
     @Select("select * from user where token = #{token}")
