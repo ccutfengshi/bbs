6e6b1c17f2cbd4f7e4f10469bae1f6cfb4c4c3a1
ccutfengshi <2524734034@qq.com>
2020-06-05 15:35:50 +0800

    添加了一个新的表和Controller，为要做的评论功能做准备

---
+++ src/main/java/com/ccut/bbs/controller/CommentController.java
@@ -0,0 +1,34 @@
+package com.ccut.bbs.controller;
+
+import com.ccut.bbs.dto.CommentDTO;
+import com.ccut.bbs.mapper.CommentMapper;
+import com.ccut.bbs.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//评论的controller
+@Controller
+public class CommentController {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @ResponseBody
+    @RequestMapping(value = "/comment",method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO) {
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setContent(commentDTO.getContent());
+        comment.setType(commentDTO.getType());
+        comment.setGmtModified(System.currentTimeMillis());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setCommentator(1);
+        comment.setLikeCount(0L);
+        commentMapper.insert(comment);
+        return null;
+    }
+}
