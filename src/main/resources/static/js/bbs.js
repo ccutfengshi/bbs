cd96f527eac9420e44642cdef5c4cc3143462244
ccutfengshi <2524734034@qq.com>
2020-06-09 16:03:50 +0800

    没有登陆状态下 点击回复有一个快速登陆

--- src/main/resources/static/js/bbs.js
+++ src/main/resources/static/js/bbs.js
@@ -8,7 +8,7 @@ function post() {
         type: "POST",
         url: "/comment",
         contentType: 'application/json',
-        data: JSON.stringify( {
+        data: JSON.stringify({
             "parentId": questionId,
             "content": content,
             "type": 1
@@ -16,10 +16,17 @@ function post() {
         success: function (response) {
             if (response.code == 200) {
                 $("#comment_section").hide();
-            }else {
-                alert(response.message);
+            } else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=Iv1.cf2d2333aba86f6d&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
-            console.log(response);
         },
         dataType: "json"
     });
